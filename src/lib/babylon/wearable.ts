34935b22870ce2ead3d9337d2f4788f25913926a
Juan Cazala <juancazala@gmail.com>
2022-09-15 16:27:34 -0300

    fix: use right type

--- src/lib/babylon/wearable.ts
+++ src/lib/babylon/wearable.ts
@@ -8,7 +8,7 @@
  */
 
 import { Color3, PBRMaterial, Scene } from '@babylonjs/core'
-import { WearableDefinition, BodyShape, RepresentationDefinition } from '@dcl/schemas'
+import { WearableDefinition, BodyShape } from '@dcl/schemas'
 import { getWearableRepresentation, isTexture, getContentUrl } from '../representation'
 import { loadAssetContainer } from './scene'
 import './toon'
@@ -20,7 +20,7 @@ export async function loadWearable(
   skin?: string,
   hair?: string
 ) {
-  const representation = getWearableRepresentation(wearable, bodyShape) as RepresentationDefinition
+  const representation = getWearableRepresentation(wearable, bodyShape)
   if (isTexture(representation)) {
     throw new Error(`The wearable="${wearable.id}" is a texture`)
   }
