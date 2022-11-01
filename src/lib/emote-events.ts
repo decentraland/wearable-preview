bdd2c4c57eaa83629a50dac8e469a6f572b0cb00
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-11-01 12:55:08 -0300

    feat: Update emote events payload type

--- src/lib/emote-events.ts
+++ src/lib/emote-events.ts
@@ -1,10 +1,11 @@
 import { IPreviewController, PreviewEmoteEventType, PreviewMessageType, sendMessage } from '@dcl/schemas'
+import { EmoteEventPayload } from '@dcl/schemas/dist/dapps/preview/preview-emote-event-payload'
 import { getParent } from './parent'
 
 export function handleEmoteEvents(controller: IPreviewController) {
   // handle an emote event by forwarding it as a message
   function handleEvent(type: PreviewEmoteEventType) {
-    controller.emote.events.on(type, (payload?: any) =>
+    controller.emote.events.on(type, (payload: EmoteEventPayload<PreviewEmoteEventType>) =>
       sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, { type, payload })
     )
   }
