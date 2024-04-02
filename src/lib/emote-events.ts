4b264264a48339444e0c555cdabe0deecc9846c5
Lautaro Petaccio <lausuper@gmail.com>
2024-04-02 17:10:27 -0300

    fix: Update schema

--- src/lib/emote-events.ts
+++ src/lib/emote-events.ts
@@ -2,18 +2,52 @@ import { IPreviewController, PreviewEmoteEventType, PreviewMessageType, sendMess
 import { EmoteEventPayload } from '@dcl/schemas/dist/dapps/preview/preview-emote-event-payload'
 import { getParent } from './parent'
 
-export function handleEmoteEvents(controller: IPreviewController) {
+export function handleEmoteEvents(controller: IPreviewController): () => void {
   // handle an emote event by forwarding it as a message
-  function handleEvent(type: PreviewEmoteEventType) {
-    controller.emote.events.on(type, (payload: EmoteEventPayload<PreviewEmoteEventType>) =>
-      sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, { type, payload })
-    )
+
+  const handleAnimationPlay = () => {
+    sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, {
+      type: PreviewEmoteEventType.ANIMATION_PLAY,
+      payload: undefined,
+    })
   }
 
-  // handle all emote event types
-  handleEvent(PreviewEmoteEventType.ANIMATION_PLAY)
-  handleEvent(PreviewEmoteEventType.ANIMATION_PAUSE)
-  handleEvent(PreviewEmoteEventType.ANIMATION_LOOP)
-  handleEvent(PreviewEmoteEventType.ANIMATION_END)
-  handleEvent(PreviewEmoteEventType.ANIMATION_PLAYING)
+  const handleAnimationPause = () => {
+    sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, {
+      type: PreviewEmoteEventType.ANIMATION_PAUSE,
+      payload: undefined,
+    })
+  }
+
+  const handleAnimationLoop = () => {
+    sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, {
+      type: PreviewEmoteEventType.ANIMATION_LOOP,
+      payload: undefined,
+    })
+  }
+
+  const handleAnimationEnd = () => {
+    sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, {
+      type: PreviewEmoteEventType.ANIMATION_END,
+      payload: undefined,
+    })
+  }
+
+  const handleAnimationPlaying = (payload: EmoteEventPayload<PreviewEmoteEventType>) => {
+    sendMessage(getParent(), PreviewMessageType.EMOTE_EVENT, { type: PreviewEmoteEventType.ANIMATION_PLAYING, payload })
+  }
+
+  controller.emote.events.on(PreviewEmoteEventType.ANIMATION_PLAY, handleAnimationPlay)
+  controller.emote.events.on(PreviewEmoteEventType.ANIMATION_PAUSE, handleAnimationPause)
+  controller.emote.events.on(PreviewEmoteEventType.ANIMATION_LOOP, handleAnimationLoop)
+  controller.emote.events.on(PreviewEmoteEventType.ANIMATION_END, handleAnimationEnd)
+  controller.emote.events.on(PreviewEmoteEventType.ANIMATION_PLAYING, handleAnimationPlaying)
+
+  return () => {
+    controller.emote.events.off(PreviewEmoteEventType.ANIMATION_PLAY, handleAnimationPlay)
+    controller.emote.events.off(PreviewEmoteEventType.ANIMATION_PAUSE, handleAnimationPause)
+    controller.emote.events.off(PreviewEmoteEventType.ANIMATION_LOOP, handleAnimationLoop)
+    controller.emote.events.off(PreviewEmoteEventType.ANIMATION_END, handleAnimationEnd)
+    controller.emote.events.off(PreviewEmoteEventType.ANIMATION_PLAYING, handleAnimationPlaying)
+  }
 }
