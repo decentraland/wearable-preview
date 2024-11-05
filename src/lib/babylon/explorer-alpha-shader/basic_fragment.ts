6aaf725c005ec56001e70be6685f461937dca8f9
Geoff Birch <geoff.birch@decentraland.org>
2024-11-05 14:43:13 +0000

    Addition of basic shader

---
+++ src/lib/babylon/explorer-alpha-shader/basic_fragment.ts
@@ -0,0 +1,12 @@
+export const customBasicFragmentShader = `#version 300 es
+precision highp float;
+
+in vec2 vUV;
+
+uniform sampler2D textureSampler;
+
+out vec4 fragColor;
+
+void main(void) {
+    fragColor = texture(textureSampler, vUV);
+}`
