d61ec7cb9b4571af864c4d7eaec0134595eed8fb
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-06-11 10:16:04 -0300

    fix: Modify routing logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AppRoutes } from './Routes'
+import { Preview } from './components/Preview'
+import { UnityPreview } from './components/UnityPreview'
 import './index.css'
 
-const basename = /^decentraland.(zone|org|today)$/.test(window.location.host) ? '/wearable-preview' : '/'
+const App = () => {
+  const search = new URLSearchParams(window.location.search)
+  const isUnityPreview = search.get('unity') !== null
+
+  return isUnityPreview ? <UnityPreview /> : <Preview />
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
-      <AppRoutes />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 )
