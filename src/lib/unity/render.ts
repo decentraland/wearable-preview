6c9931e8c0bc1df5c2cb77c796921e50d8266d71
Rocío <69587750+RocioCM@users.noreply.github.com>
2026-08-13 11:41:17 -0300

    Update render.ts
    Signed-off-by: Rocío <69587750+RocioCM@users.noreply.github.com>

--- src/lib/unity/render.ts
+++ src/lib/unity/render.ts
@@ -28,7 +28,7 @@ type AangBuildConfig = {
 }
 
 // UPDATE THIS URL WITH LATEST AANG BUILD PREVIEW URL:
-const REMOTE_BASE_URL = 'https://aang-renderer-git-fix-marketplace-zero-urns-decentraland1.vercel.app/'
+const REMOTE_BASE_URL = 'https://aang-renderer-jrb9j75a6-decentraland1.vercel.app/'
 
 const COMPRESSED_AANG_BUILD: AangBuildConfig = {
   dataUrl: '/unity/Build/aang-renderer.data.br',
